22ab82034a6c265fd9526e3575e2669018e056d0
Sathishkumar R <rsathishtechit@gmail.com>
2025-11-08 06:37:23 +0530

    updated contents

--- forge.config.ts
+++ forge.config.ts
@@ -30,8 +30,8 @@ const config: ForgeConfig = {
       background: "assets/icons/dmg-background.png",
       contents: [
         { x: 448, y: 344, type: "link", path: "/Applications" },
-        { x: 192, y: 344, type: "file", path: "Nilaa Player.app" }
-      ]
+        { x: 192, y: 344, type: "file", path: "Nilaa Player.app" },
+      ],
     }),
     new MakerRpm({
       options: {
